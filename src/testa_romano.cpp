66cbe133d6f499b58d7b3c15cd45868eaeca7b1c
Rodrigo-SS <rosyss92@gmail.com>
2017-04-24 10:41:56 -0300

    inserindo .cpp

--- src/testa_romano.cpp
+++ src/testa_romano.cpp
@@ -1,87 +1,69 @@
 #include <gtest/gtest.h>
 #include <romano.h>
 
-// int main(int argc, char const *argv[])
-// {
-// char palavra[30];
-// int i=0, c;
-	
-// 	printf("Digite um numero romano: ");
-// 	scanf("%s", palavra);
+char palavra[30];
 
-// 	while(palavra[i]){
-//       palavra[i] = toupper(palavra[i]);
-//       i++;
-// 	}
-
-// 	c=letras_diferentes(palavra);
-// 	if (c==-1){
-// 		printf("Numero invalido!\n");
-// 		exit(0);
-// 	}
-
-// 	c=quatro_letras(palavra);
-// 	if (c==-1){
-// 		printf("Sera\n");
-// 		printf("Numero invalido!\n");
-// 		exit(0);
-// 	}
-
-// 	c=casos_duplos(palavra);
-// 	if (c==-1){
-// 		printf("Numero invalido!\n");
-// 		exit(0);
-// 	}
-
-// 	c=casos_especiais(palavra);
-// 	if (c==-1){
-// 		printf("Numero invalido!\n");
-// 		exit(0);
-// 	}
-
-// 	c=verificacao(palavra);
-// 	if (c==-1){
-// 		printf("Numero invalido!\n");
-// 		exit(0);
-// 	}
-
-// 	return 0;
-// }
-
-//Testa os Numeros basicos 
+//Testa as letras romanas
 TEST(RomanTest, Letras){
 	EXPECT_EQ(0, letras_diferentes((char*)"I"));
-	EXPECT_EQ(0, letras_diferentes((char*)"V"));
-	EXPECT_EQ(0, letras_diferentes((char*)"X"));
-	EXPECT_EQ(-1, letras_diferentes((char*)"s"));
-	EXPECT_EQ(-1, letras_diferentes((char*)"t"));
-	EXPECT_EQ(-1, letras_diferentes((char*)"R"));
+	EXPECT_EQ(0, letras_diferentes((char*)"III"));
+	EXPECT_EQ(0, letras_diferentes((char*)"XII"));
+	EXPECT_EQ(-1, letras_diferentes((char*)"S"));
+	EXPECT_EQ(-1, letras_diferentes((char*)"vit"));
+	EXPECT_EQ(-1, letras_diferentes((char*)"DCU"));
 }
 
-// TEST(RomanTest, quatro){
-// 	EXPECT_EQ(0, quatro_letras((char*)"XVII"));
-// 	EXPECT_EQ(0, quatro_letras((char*)"III"));
-// 	EXPECT_EQ(0, quatro_letras((char*)"CCC"));
-// 	EXPECT_EQ(-1, quatro_letras((char*)"IIII"));
-// 	EXPECT_EQ(-1, quatro_letras((char*)"VVVV"));
-// 	EXPECT_EQ(-1, quatro_letras((char*)"CCCC"));
-// }
+//testa se há quatro letras iguais
+TEST(RomanTest, Quatro){
+	EXPECT_EQ(0, quatro_letras((char*)"XVII"));
+	EXPECT_EQ(0, quatro_letras((char*)"III"));
+	EXPECT_EQ(0, quatro_letras((char*)"CCC"));
+	EXPECT_EQ(-1, quatro_letras((char*)"DCIIII"));
+	EXPECT_EQ(-1, quatro_letras((char*)"VVVV"));
+	EXPECT_EQ(-1, quatro_letras((char*)"CCCC"));
+}
 
-// TEST(RomanTest, Letras){
-// 	EXPECT_EQ(0, letras_diferentes((char*)"I"));
-// 	EXPECT_EQ(0, letras_diferentes((char*)"V"));
-// 	EXPECT_EQ(0, letras_diferentes((char*)"X"));
-// 	EXPECT_EQ(-1, letras_diferentes((char*)"s"));
-// 	EXPECT_EQ(-1, letras_diferentes((char*)"t"));
-// 	EXPECT_EQ(-1, letras_diferentes((char*)"R"));
-// }
+//testa se ha duplicidade
+TEST(RomanTest, Duplicidade){
+	EXPECT_EQ(0, casos_duplos((char*)"III"));
+	EXPECT_EQ(0, casos_duplos((char*)"XLII"));
+	EXPECT_EQ(0, casos_duplos((char*)"MMDCXXXIII"));
+	EXPECT_EQ(-1, casos_duplos((char*)"VVI"));
+	EXPECT_EQ(-1, casos_duplos((char*)"LLVII"));
+	EXPECT_EQ(-1, casos_duplos((char*)"DDXL"));
+}
 
-// TEST(RomanTest, Letras){
-// 	EXPECT_EQ(0, letras_diferentes((char*)"I"));
-// 	EXPECT_EQ(0, letras_diferentes((char*)"V"));
-// 	EXPECT_EQ(0, letras_diferentes((char*)"X"));
-// 	EXPECT_EQ(-1, letras_diferentes((char*)"s"));
-// 	EXPECT_EQ(-1, letras_diferentes((char*)"t"));
-// 	EXPECT_EQ(-1, letras_diferentes((char*)"R"));
-// }
+//testa as funçoes de verificacao e arabico
+TEST(RomanTest, Verificacao_e_Arabico){
+	EXPECT_EQ(190, verificacao((char*)"CXC"));
+	EXPECT_EQ(1919, verificacao((char*)"MCMXIX"));
+	EXPECT_EQ(999, verificacao((char*)"CMXCIX"));
+	EXPECT_EQ(-1, verificacao((char*)"IIV"));
+	EXPECT_EQ(-1, verificacao((char*)"DCVLMI"));
+	EXPECT_EQ(-1, verificacao((char*)"MMCDDI"));
+}
 
+//testa os casos especiais que sao invalidos(ex.: XLX, XCX, CDC, CMC, VIX)
+TEST(RomanTest, Casos_especiais){
+	EXPECT_EQ(0, casos_especiais((char*)"CXC"));
+	EXPECT_EQ(0, casos_especiais((char*)"XIX"));
+	EXPECT_EQ(0, casos_especiais((char*)"MCM"));
+	EXPECT_EQ(-1, casos_especiais((char*)"VIX"));
+	EXPECT_EQ(-1, casos_especiais((char*)"XCX"));
+	EXPECT_EQ(-1, casos_especiais((char*)"CDC"));
+}
+
+//resultado final dos numeros romanos
+TEST(RomanTest, Resultado){
+	EXPECT_EQ(1677, controller((char*)"mdclxxvii"));
+	EXPECT_EQ(1999, controller((char*)"mcmXCIX"));
+	EXPECT_EQ(642, controller((char*)"dcxlii"));
+	EXPECT_EQ(-1, controller((char*)"iiv"));
+	EXPECT_EQ(-1, controller((char*)"dcti"));
+	EXPECT_EQ(-1, controller((char*)"mmmmdc"));
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
